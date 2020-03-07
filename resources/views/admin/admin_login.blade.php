0b03fb1243060b5807ec57f5b7da694aa0842e49
HervalMata <hervalmataamparo@gmail.com>
2020-03-07 01:54:57 -0300

    Set Admin dashboard, footer, header

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -7,32 +7,32 @@
     <link rel="stylesheet" href="{{ asset('css/backend_css/bootstrap.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/backend_css/bootstrap-responsive.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/backend_css/matrix-login.css') }}" />
-    <link href="{{ asset('fonts/backend_fonts/font-awesome/css/font-awesome.css') }}" rel="stylesheet" />
+    <link href="{{ asset('fonts/backend_fonts/css/font-awesome.css') }}" rel="stylesheet" />
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
 
 </head>
 <body>
 <div id="loginbox">
-    <form id="loginform" class="form-vertical" method="post" action="{{ url('admin') }}">
+    <form id="loginform" class="form-vertical" method="POST" action="{{ url('admin') }}">
         {{ csrf_field() }}
         <div class="control-group normal_text"> <h3><img src="{{ asset('images/backend_images/logo.png') }}" alt="Logo" /></h3></div>
         <div class="control-group">
             <div class="controls">
                 <div class="main_input_box">
-                    <span class="add-on bg_lg"><i class="icon-user"> </i></span><input type="text" name="email" placeholder="Email" />
+                    <span class="add-on bg_lg"><i class="icon-user"> </i></span><input id="email" type="text" name="email" placeholder="Email" />
                 </div>
             </div>
         </div>
         <div class="control-group">
             <div class="controls">
                 <div class="main_input_box">
-                    <span class="add-on bg_ly"><i class="icon-lock"></i></span><input type="password" name="password" placeholder="Senha" />
+                    <span class="add-on bg_ly"><i class="icon-lock"></i></span><input id="password" type="password" name="password" placeholder="Senha" />
                 </div>
             </div>
         </div>
         <div class="form-actions">
             <span class="pull-left"><a href="#" class="flip-link btn btn-info" id="to-recover">Esqueceu a senha?</a></span>
-            <span class="pull-right"><a type="submit" value="Login" class="btn btn-success" > Entrar</a></span>
+            <span class="pull-right"><input type="submit" value="Entrar" class="btn btn-success" /></span>
         </div>
     </form>
     <form id="recoverform" action="#" class="form-vertical">
